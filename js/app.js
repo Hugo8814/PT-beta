30f91ed22b24f7d50a94b1f398def0a46a359472
Luke Horvath <lukehorvath@outlook.com>
2023-12-12 02:29:06 +0000

    bunch of changes -- added javascript listener to logo on other pages to return to splash -- changed browser demo button to javascript to fix occassional non-responsive behaviour

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
+let btn = document.getElementById("btn")
 
+btn.addEventListener("click", function(){
+    console.log("clicked button")});
+
+let btnDemo = document.getElementById("btn-demo")
+
+btnDemo.addEventListener("click", function(){
+    window.location.href=('html/workout.html')
+    console.log("clicked button")});
