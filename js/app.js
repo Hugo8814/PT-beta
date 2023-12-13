079ce4496b7e76ca0cc53713c2d3130ecc888501
Luke Horvath <lukehorvath@outlook.com>
2023-12-13 01:19:54 +0000

    added kickstarter link js

--- js/app.js
+++ js/app.js
@@ -8,3 +8,9 @@ let btnDemo = document.getElementById("btn-demo")
 btnDemo.addEventListener("click", function(){
     window.location.href=('html/workout.html')
     console.log("clicked button")});
+
+let kickstartBar = document.getElementById("kickstarterBar")
+
+kickstartBar.addEventListener("click", function(){
+    window.location.href="https://www.kickstarter.com/"
+})
